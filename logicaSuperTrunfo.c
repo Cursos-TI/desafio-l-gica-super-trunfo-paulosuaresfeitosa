8a69ff20857ebe4c452f9a22385c2082fef1d22b
paulosuaresfeitosa <paulosuaresfeitosa@gmail.com>
2025-01-06 22:35:49 +0000

    new commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
+criei o título
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main() {printf ("Desafio Super Trunfo 1");
+
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+
 
     
     // Cadastro das Cartas:
